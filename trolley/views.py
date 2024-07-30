5735f0c3226e4276f1de7860138d2453e7bb3b6b
Beatriz <beaultfernandes@gmail.com>
2024-07-30 14:33:14 -0300

    criação das Class-Based Views

--- trolley/views.py
+++ trolley/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'trolley/product_list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'trolley/product_detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'trolley/product_form.html'
+    success_url = reverse_lazy('product-list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'trolley/product_form.html'
+    success_url = reverse_lazy('product-list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'trolley/product_confirm_delete.html'
+    success_url = reverse_lazy('product-list')
